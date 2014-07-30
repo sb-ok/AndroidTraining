70e7aaf2fbd60c0b4e6069d78fb09bb3073fb600
Sergey <nels.brain@gmail.com>
2014-07-30 21:59:33 +0400

    Ориентация: работаю над развертыванием экрана с кнопки

--- OrientationApp/src/main/java/ru/sbdev/orientation/MainActivity.java
+++ OrientationApp/src/main/java/ru/sbdev/orientation/MainActivity.java
@@ -1,18 +1,36 @@
 package ru.sbdev.orientation;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button btn;
+    static final String ORIENTATION_PORTRAIT = "Портретный режим";
+    static final String ORIENTATION_LANDSCAPE = "Альбомный режим";
+
+    //Определяем изменение ориентации экрана
+    boolean mState = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+       // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // запускает приложение в альбомной ориентации
+
+        btn = (Button) findViewById(R.id.changeOrintation);
+
+        // установим положение по умолчанию
+        btn.setText(ORIENTATION_LANDSCAPE);
     }
 
 
@@ -43,4 +61,53 @@ public class MainActivity extends ActionBarActivity {
         else
             return "лажа";
     }
+
+    private String  mOrientation = "";
+    private boolean isLandscapeMode(Activity activity)
+    {
+        int width = activity.getWindowManager().getDefaultDisplay().getWidth();
+        int height = activity.getWindowManager().getDefaultDisplay().getHeight();
+
+        boolean isLandscape = width > height;
+
+        if(isLandscape)
+            mOrientation = "Альбомная";
+        else mOrientation = "Портретная";
+
+        return isLandscape;
+    }
+
+    private String getRotateOrientation()
+    {
+        int rotate = getWindowManager().getDefaultDisplay().getRotation();
+        switch (rotate){
+            case Surface.ROTATION_0:
+                return "Не поварачивали";
+            case Surface.ROTATION_90:
+                return "Повернули на 90 градусов по часовой стрелке";
+            case Surface.ROTATION_180:
+                return "Повернули на 180 градусов";
+            case Surface.ROTATION_270:
+                return "Повернули на 270 градусов";
+            default:
+                return "Не понятно куда повернули";
+        }
+    }
+
+    public void onClick(View v)
+    {
+        // если mState FALSE - переключаемся в альбомный режим
+        if(!mState){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            btn.setText(ORIENTATION_LANDSCAPE);
+        }
+        // если mState TRUE - переключаемся в портретный режим
+        else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            btn.setText(ORIENTATION_PORTRAIT);
+
+            mState = !mState;
+        }
+    }
+
 }
